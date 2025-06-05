69d3ce95ba67126de8028d960de85e823b95bf30
Dan Carney <dtcarney08@gmail.com>
2025-06-05 04:57:28 -0500

    Week6Task1: Updated Simple Table

--- force-app/main/default/lwc/simpleTable/simpleTable.js
+++ force-app/main/default/lwc/simpleTable/simpleTable.js
@@ -2,46 +2,49 @@ import { LightningElement } from 'lwc';
 
 export default class SimpleTable extends LightningElement {
 
-column1 = 'Opportunity Name';
-column2 = 'Stage';
-column3 = 'Amount';
-column4 = 'Close Date';
+columns = [ 
+    {name: 'Opportunity Name'},
+    {name: 'Stage'},
+    {name: 'Amount'},
+    {name: 'Close Date'}];
 
 
-opp1 = {
-    name: 'Acme Inc',
-    stage: 'Prospecting',
-    amount: '$55'+','+'5555',
-    closedate: '06/02/2025'
-}
 
-opp2 = {
-    name : 'Global co',
-    stage: 'Closed Won',
-    amount: '15'+','+'555',
-    closedate: '09/05/2025'
-}
+opportunities  = [
+    {
+        name: 'Acme Inc',
+        stage: 'Prospecting',
+        amount: '$55'+','+'5555',
+        closedate: '06/02/2025'
+    },
 
-opp3 = {
-    name: 'Vandelay Industries',
-    stage: 'Closed Lost',
-    amount: '230',
-    closedate: '01/02/2026'
-}
+    {
+        name : 'Global co',
+        stage: 'Closed Won',
+        amount: '15'+','+'555',
+        closedate: '09/05/2025'
+    },
 
-opp4 = {
-    name: 'Kramerica',
-    stage: 'Negotiation',
-    amount: '25'+','+'555',
-    closedate: '10/05/2025'
-}
+    {
+        name: 'Vandelay Industries',
+        stage: 'Closed Lost',
+        amount: '230',
+        closedate: '01/02/2026'
+    },
 
-opp5 = {
-    name: 'Kruger Sanding',
-    stage: 'Closing',
-    amount: '25'+','+'211',
-    closedate: '7/02/2025'
-}
+    {
+        name: 'Kramerica',
+        stage: 'Negotiation',
+        amount: '25'+','+'555',
+        closedate: '10/05/2025'
+    },
+
+    {
+        name: 'Kruger Sanding',
+        stage: 'Closing',
+        amount: '25'+','+'211',
+        closedate: '7/02/2025'
+    }];
 
 
 
